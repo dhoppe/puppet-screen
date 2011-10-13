9427a44b4156f63064af05454736242974273f1f
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2011-10-13 22:26:24 +0200

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'dhoppe-screen'
+version '0.0.1'
+
+author 'Dennis Hoppe'
+license 'GPLv2'
+project_page 'http://github.com/dhoppe/puppet-screen'
+source 'git://github.com/dhoppe/puppet-screen'
+summary 'Screen Module'
+description 'Screen Module for Debian, Ubuntu'
---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+class screen {
+	file { "/etc/screenrc":
+		owner   => root,
+		group   => root,
+		mode    => 0644,
+		source  => "puppet:///modules/screen/common/etc/screenrc",
+		require => Package["screen"],
+	}
+
+	package { "screen":
+		ensure => present,
+	}
+}
+
+# vim: tabstop=3
