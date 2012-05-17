ee1eb8726f29be749be7389db46335e3482d737d
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-05-17 15:06:50 +0200

    Fix several issues mentioned at Puppet style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
-class screen {
+class screen inherits screen::params {
   file { '/etc/screenrc':
-    owner   => root,
-    group   => root,
+    owner   => 'root',
+    group   => 'root',
     mode    => '0644',
     source  => 'puppet:///modules/screen/common/etc/screenrc',
     require => Package['screen'],
