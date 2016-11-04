66ed18e5e6c6fdb3b0ae9843e01cbef1b6b9440c
Dennis Hoppe <github@debian-solutions.de>
2016-11-04 21:57:26 +0100

    Migrate to rspec-puppet-facts

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,21 +1,19 @@
 require 'spec_helper'
 
 describe 'screen', type: :class do
-  ['Debian'].each do |osfamily|
-    let(:facts) do
-      {
-        osfamily: osfamily
-      }
-    end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
 
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_anchor('screen::begin') }
-    it { is_expected.to contain_class('screen::params') }
-    it { is_expected.to contain_class('screen::install') }
-    it { is_expected.to contain_class('screen::config') }
-    it { is_expected.to contain_anchor('screen::end') }
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.to contain_anchor('screen::begin') }
+      it { is_expected.to contain_class('screen::params') }
+      it { is_expected.to contain_class('screen::install') }
+      it { is_expected.to contain_class('screen::config') }
+      it { is_expected.to contain_anchor('screen::end') }
 
-    context "on #{osfamily}" do
       describe 'screen::install' do
         context 'defaults' do
           it do
